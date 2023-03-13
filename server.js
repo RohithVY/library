842d0f6a6db720e756b1e71c33395c7fa1e8d992
RohithVY <venkatrohith1998@gmail.com>
2023-03-13 21:42:53 +0530

    layout files and db connection was added

--- server.js
+++ server.js
@@ -2,17 +2,25 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 
 const indexRouter = require('./routes/index.js')
+const path = require('path');
+const dotenv = require('dotenv').config();
+const colors = require('colors');
+const connectDB = require('./config/db')
 
 const app =  express();
 const port = process.env.PORT || 5000
 
-app.set('view-engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
+connectDB()
+
 
 app.use(expressLayouts)
 // Where our public files going to be - styles, javascript
-app.use(express.static('public'))
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('layout', 'layouts/layout')
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter)
 
 app.listen(port, ( )=>{
